b909ba1a77aef4e6156e8370138d211b8b3f9b7e
danielmassey <danielmassey1@gmail.com>
2018-10-15 21:42:05 -0500

    test02

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -2,15 +2,22 @@
 
 sudo apt-mark hold grub*
 sudo apt-mark hold kernel*
+sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt -y upgrade
+apt-cache policy docker-ce
 sudo apt -y install openjdk-8-jdk
-sudo apt -y install jenkins
+sudo apt -y install jenkins docker-ce docker-compose
+sudo usermod -aG docker jenkins
+sudo usermod -aG docker  ubuntu
 
 sudo systemctl start jenkins
 sudo systemctl status jenkins
+sudo systemctl status docker
 
 sudo ufw allow 8080
 sudo ufw status
