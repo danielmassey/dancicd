8f29d197d93437f8affd5b753e9d930d0f4d5756
Daniel Massey <danielmassey1@gmail.com>
2018-10-27 13:05:34 -0500

    docker stuff

---
+++ app/DynamoDBinit.go
@@ -0,0 +1,5 @@
+
+
+
+
+//https://docs.aws.amazon.com/sdk-for-go/v1/developer-guide/dynamo-example-update-table-item.html
--- jenkins/jenkins_init.sh
+++ jenkins/jenkins_init.sh
@@ -12,9 +12,24 @@ sudo apt-get update
 sudo apt -y upgrade
 apt-cache policy docker-ce
 sudo apt -y install openjdk-8-jdk awscli
-sudo apt -y install jenkins=2.89.4 docker-ce docker-compose
+sudo apt -y install jenkins=2.89.4 docker-ce docker-compose rng-tools golang-go
 sudo usermod -aG docker jenkins
 sudo usermod -aG docker ubuntu
+
+#Needed for secretservice  Ubuntu issue https://github.com/docker/compose/issues/6023
+cat >gpg2.tmp <<EOF
+Key-Type: default
+Subkey-Type: default
+Name-Real: Daniel Massey
+Name-Comment: https://github.com/danielmassey/dancicd
+Name-Email: danielmassey@danielmassey.com
+Expire-Date: 0
+Passphrase: password
+EOF
+
+gpg2 --full-generate-key --batch gpg2.tmp
+rm gpg2.tmp
+pass init `gpg2 --list-secret-keys | grep uid -B 1 | head -n 1`
 
 sudo systemctl start jenkins
 sudo systemctl status jenkins
